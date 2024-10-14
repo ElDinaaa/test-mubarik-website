2d597aa87ba83113decc75182152bce84a290841
Dina El <dinael@Dinas-MacBook-Pro.local>
2024-10-14 14:02:14 +0300

    Clear cookies is true now

--- src/main/java/commonActions/Config.java
+++ src/main/java/commonActions/Config.java
@@ -1,7 +1,7 @@
 package commonActions;
 
 public class Config {
-    public static final Boolean CLEAR_COOKIES = false;
+    public static final Boolean CLEAR_COOKIES = true;
     public static final Boolean HOLD_BROWSER_OPEN = false;
     public static final Boolean CLEAR_REPORTS_DIR = true;
 }
